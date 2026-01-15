dc4f84e29d54f6f54c70c1277054967a308f2c51
Stefan Bischof <stbischof@bipolis.org>
2026-01-15 22:38:26 +0100

    fix license header
    Signed-off-by: Stefan Bischof <stbischof@bipolis.org>

--- core/src/main/java/org/eclipse/daanse/rolap/common/CacheControlImpl.java
+++ core/src/main/java/org/eclipse/daanse/rolap/common/CacheControlImpl.java
@@ -20,21 +20,7 @@
  *
  * Contributors after Fork in 2023:
  *   SmartCity Jena - initial
-  *
- * ---- All changes after Fork in 2023 ------------------------
  *
- * Project: Eclipse daanse
- *
- * Copyright (c) 2023 Contributors to the Eclipse Foundation.
- *
- * This program and the accompanying materials are made
- * available under the terms of the Eclipse Public License 2.0
- * which is available at https://www.eclipse.org/legal/epl-2.0/
- *
- * SPDX-License-Identifier: EPL-2.0
- *
- * Contributors after Fork in 2023:
- *   SmartCity Jena - initial
  */
 
 
